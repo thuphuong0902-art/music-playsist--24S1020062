6a3b0d797a9284dd97551cba0d09a5f8bd8ae77a
thuphuong0902-art <ribithuphuong0902@gmail.com>
2025-12-02 11:15:50 +0700

    Add feature/add-song function

--- Playlist.py
+++ Playlist.py
@@ -9,19 +9,11 @@ def add_song(title, artist, duration):
     songs.append(song)
     return song
 
-def view_playlist():
-    if not songs:
-        print("Playlist hiện đang trống.")
-        return
-        
-    print("Danh sách bài hát trong playlist:")
-    for index, song in enumerate(songs, start=1):
-        print(f"{index}. {song['title']} - {song['artist']} ({song['duration']}s)")
-        
-
-# Test nhanh:
+# Ví dụ chạy thử
 if __name__ == "__main__":
-    add_song("Lạc Trôi", "Sơn Tùng MTP", 240)
-    add_song("Nơi Này Có Anh", "Sơn Tùng MTP", 230)
-
-    view_playlist()
+    t = input("Tên bài hát: ")
+    a = input("Ca sĩ: ")
+    d = int(input("Thời lượng (giây): "))
+    
+    added = add_song(t, a, d)
+    print("Đã thêm:", added)
