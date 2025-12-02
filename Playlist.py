9e93f5e28b98633d6b21fd1adaaa42270af0b44f
thuphuong0902-art <ribithuphuong0902@gmail.com>
2025-12-02 11:45:40 +0700

    add feature/search-artist function

--- Playlist.py
+++ Playlist.py
@@ -17,11 +17,24 @@ def view_playlist():
     print("Danh sách bài hát trong playlist:")
     for index, song in enumerate(songs, start=1):
         print(f"{index}. {song['title']} - {song['artist']} ({song['duration']}s)")
-        
+
+def search_by_artist(artist_name):
+    results = [song for song in songs if song["artist"].lower() == artist_name.lower()]
+
+    if not results:
+        print(f"Không tìm thấy bài hát nào của ca sĩ: {artist_name}")
+        return
+
+    print(f"Các bài hát của '{artist_name}':")
+    for index, song in enumerate(results, start=1):
+        print(f"{index}. {song['title']} ({song['duration']}s)")
+
 
 # Test nhanh:
 if __name__ == "__main__":
     add_song("Lạc Trôi", "Sơn Tùng MTP", 240)
     add_song("Nơi Này Có Anh", "Sơn Tùng MTP", 230)
+    add_song("Em Của Ngày Hôm Qua", "Sơn Tùng MTP", 215)
+    add_song("Tháng Tư Là Lời Nói Dối Của Em", "Hà Anh Tuấn", 300)
 
-    view_playlist()
+    search_by_artist("Sơn Tùng MTP")
